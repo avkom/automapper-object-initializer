ca661f0f8956027a6ead63186afc5e96f1cce46f
Oleksandr Komendant <avkomq@gmail.com>
2019-03-29 19:03:00 -0400

    Add test project

---
+++ src/AutoMapper.ObjectInitializer.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Should;
+using Xunit;
+
+namespace AutoMapper.ObjectInitializer.UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            1.ShouldEqual(1);
+        }
+    }
+}

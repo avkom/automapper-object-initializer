7f530271c97364bd92acfda746ff063f8b55f2ce
Oleksandr Komendant <avkomq@gmail.com>
2019-03-29 19:20:56 -0400

    Add TestForMember

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automapper-object-initializer
 This library provides ability to use object initializer expressions to configure member mappings in AutoMapper
+
+Inspired by https://habr.com/en/post/338968/. See https://github.com/Birbone/FlashMapper.
--- src/AutoMapper.ObjectInitializer.UnitTests/Class1.cs
+++ src/AutoMapper.ObjectInitializer.UnitTests/Class1.cs
@@ -5,10 +5,38 @@ namespace AutoMapper.ObjectInitializer.UnitTests
 {
     public class Class1
     {
-        [Fact]
-        public void Test()
+        public class UserModel
         {
-            1.ShouldEqual(1);
+            public string Name { get; set; }
+        }
+
+        public class UserEntity
+        {
+            public string Title { get; set; }
+        }
+
+        [Fact]
+        public void TestForMember()
+        {
+            // Arrange
+            MapperConfiguration configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<UserModel, UserEntity>()
+                    .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Name));
+            });
+            IMapper mapper = configuration.CreateMapper();
+            UserModel userModel = new UserModel
+            {
+                Name = "Name"
+            };
+
+            // Act
+            UserEntity userEntity = mapper.Map<UserModel, UserEntity>(userModel);
+
+            // Assert
+            userEntity.ShouldNotBeNull();
+            userEntity.Title.ShouldEqual(userModel.Name);
+            configuration.AssertConfigurationIsValid();
         }
     }
 }
